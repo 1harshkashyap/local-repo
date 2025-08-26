dfc6d091001e9dc103e84a484e36eb305689061b
1harshkashyap <harshkashyap773706@gmail.com>
2025-08-26 18:40:14 +0530

    ok

--- remainder.c
+++ remainder.c
@@ -5,6 +5,5 @@ int main(){
     scanf("%d %d",&a,&b);
     c=a/b;
     printf("THE VALUE OF REMAINDER IS:%d ",a-(b*c));//WE COULD ALSO DECLARE A NEW VARIABLE TO STORE THE VALUE OF REMAINDER
-
     return 0;
 }
