9bafc7e8a563abe989d76ebfd6d04719bfac393c
1harshkashyap <harshkashyap773706@gmail.com>
2025-08-26 16:33:54 +0530

    DONE

---
+++ area.c
@@ -0,0 +1,12 @@
+//C PROGRAMME FOR PERIMETER OF A RECTANGLE OF GIVEN DIMENSIONS
+#include <stdio.h>
+int main(){
+    float a,b ;
+    printf ("ENTER LENGTH:");
+    scanf("%f",&a);
+    printf ("ENTER BREADTH:");
+    scanf("%f",&b);
+    printf("THE PERIMETER OF RECTANGLE IS: %f",(2*(a+b)));
+
+    return 0;
+}
